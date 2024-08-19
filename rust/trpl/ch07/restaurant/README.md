513cd2a9e68ccefb42632fcf1f68030ee3713f75
韩剑 <hanjian@umu.com>
2024-08-19 14:50:41 +0800

    rust

--- rust/trpl/ch07/restaurant/README.md
+++ rust/trpl/ch07/restaurant/README.md
@@ -35,4 +35,57 @@ crate
 ## 使用 pub 关键字暴露路径
 
 
-## 
+## 使用 super 开始的相对路径
+
+```rust
+
+fn deliver_order() {}
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // 使用 super 调用父模块的函数
+    }
+
+    fn cook_order() {}
+}
+```
+
+## 创建共有的结构体和枚举
+
+```rust
+mod front_and_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+
+fn eat_at_restaurant() {
+    let mut meal = front_and_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // 错误，seasonal_fruit 是私有的
+
+    // 枚举公有后枚举成员都公有
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+
+```
--- rust/trpl/ch07/restaurant/src/lib.rs
+++ rust/trpl/ch07/restaurant/src/lib.rs
@@ -1,4 +1,5 @@
 mod front_and_house {
+
     pub mod hosting {
         pub fn add_to_wait_list() {} // 使用 pub 将函数公有
         pub fn seat_at_table() {}
@@ -11,9 +12,46 @@ mod front_and_house {
     }
 }
 
+fn deliver_order() {}
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // 使用 super 调用父模块的函数
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
 fn eat_at_restaurant() {
     // 绝对路径
     crate::front_and_house::hosting::add_to_wait_list();
     // 相对路径
     front_and_house::hosting::add_to_wait_list();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // 错误，seasonal_fruit 是私有的
+    //
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
