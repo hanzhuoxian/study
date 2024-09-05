99199566528d6ddc267871cd721037e5db75dc60
Jian Han <hanjian@umu.com>
2024-09-05 08:04:02 +0800

    rust

---
+++ rust/trpl/ch14/flora-sum/src/lib.rs
@@ -0,0 +1,12 @@
+/// sum
+/// sum two number
+/// ```
+/// use flora_sum::sum;
+/// let x = 5;
+/// let y = 2;
+/// 
+/// assert_eq!(7, sum(x, y));
+/// ``` 
+pub fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
