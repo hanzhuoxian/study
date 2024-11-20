bdeb9b246fd47730bef2e06586e4e1425fe0beb5
Jian Han <hanjian@umu.com>
2024-11-20 23:36:40 +0800

    rust-tao

---
+++ rust/rust-tao-work/ch02/exp/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    pub fn answer() -> () {
+        let a = 40;
+        let b = 2;
+        assert_eq!(sum(a, b), 43)
+    }
+
+    pub fn sum(a: i32, b: i32) -> i32 {
+        a + b
+    }
+    answer();
+
+    fn temp() -> i32 {
+        1
+    }
+
+    let x = temp();
+    // temp() = &x
+}
---
+++ rust/rust-tao-work/ch02/movevar/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let place1 = "hello";
+    let place2 = "hello".to_string();
+    let other  = place1;
+    println!("other {:?}", other);
+    let other = place2;
+    println!("other {:?}", other);
+}
