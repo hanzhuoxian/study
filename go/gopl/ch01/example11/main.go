f164523beb21cf27c65aa295cca5bab0f48c5ee6
Jian Han <hanjian@umu.com>
2025-04-10 21:07:03 +0800

    ch

--- go/gopl/ch01/example11/main.go
+++ go/gopl/ch01/example11/main.go
@@ -1,3 +1,9 @@
-package mai
+package main
+
+import "fmt"
 
 // 网站没有响应
+
+func main() {
+	fmt.Println("Hello, World!")
+}
--- go/gopl/go.mod
+++ go/gopl/go.mod
@@ -1,3 +1,3 @@
 module github.com/hanzhuoxian/study/go/gopl
 
-go 1.24.1
+go 1.22.12
