bac380c483ba12b8f537628c06bcbd7830192473
Jian Han <hanjian@umu.com>
2026-07-17 00:41:37 +0900

    map

---
+++ go/gopl/ch04/charcount/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)
+	var utfLen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+
+		if r == utf8.RuneError && n == 1 {
+			invalid++
+			continue
+		}
+
+		counts[r]++
+		utfLen[n]++
+	}
+
+	fmt.Printf("rune\tcount\n")
+	for r, n := range counts {
+		fmt.Printf("%q\t%d\n", r, n)
+	}
+
+	fmt.Printf("\nlen\tcount\n")
+	for i, n := range utfLen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
--- go/gopl/ch04/map_def/main.go
+++ go/gopl/ch04/map_def/main.go
@@ -1,8 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
+	// 创建 1
+	ages := make(map[string]int)
+	ages["alice"] = 31
+	ages["charlie"] = 34
+
+	// 创建 2 等价与 创建 1
+	ages1 := map[string]int{
+		"alice":   31,
+		"charlie": 34,
+	}
+
+	fmt.Println(ages1)
+
+	// 删除
+	delete(ages1, "alice")
+	fmt.Println(ages1)
+
+	// 不能进行取地址
+	// _ = &ages1["alice"]
+
+	// 遍历全部元素
+	fmt.Println("for range ages start")
+	for k, v := range ages {
+		fmt.Println(k, v)
+	}
+	fmt.Println("for range ages end")
+
+	// map 有序输出
+	fmt.Println("order by map start")
+	names := make([]string, len(ages))
+	for k := range ages {
+		names = append(names, k)
+	}
+	sort.Strings(names)
+
+	for _, n := range names {
+		fmt.Println(n, ages[n])
+	}
+
+	fmt.Println("order by map end")
+
 	var m map[int]int
 	fmt.Println(m == nil)
 	fmt.Println(len(m) == 0)
@@ -15,7 +59,11 @@ func main() {
 	if i, ok := m1[1]; ok {
 		fmt.Println(i)
 	}
+
 	if i, ok := m1[2]; !ok {
 		fmt.Println("no", i)
 	}
+
+	// map 也不能使用等于号进行比较
+
 }
